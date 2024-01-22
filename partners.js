82b06fe0d474b6652284d1d993c616e0fe8ad34f
davidboll <david.boll@me.com>
2024-01-22 13:27:15 +0100

    Merge with feature/partner-card

---
+++ partners.js
@@ -0,0 +1,11 @@
+$(() => {
+  class Partner {
+    constructor(id, name, email, logo, jobpost) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.logo = logo;
+      this.jobpost = jobpost;
+    }
+  }
+});
