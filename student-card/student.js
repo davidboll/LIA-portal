feac4ac9d0a1d0639e7d5f130a1cad3a212e9b56
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-01-22 14:31:47 +0100

    added constructor

---
+++ student-card/student.js
@@ -0,0 +1,34 @@
+$(() => {
+    function Student (
+        ID,
+        name,
+        age,
+        location,
+        education,
+        languages,
+        bio,
+        goals,
+        skills,
+        email,
+        phone,
+        social,
+    ) {
+        this.ID = ID;
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.education = education;
+        this.languages = languages;
+        this.bio = bio;
+        this.goals = goals;
+        this.skills = skills;
+        this.email = email;
+        this.phone = phone;
+        this.social =social;
+    }
+
+    
+  
+   
+  });
+  
