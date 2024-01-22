641e16cb86f9a1e5380dc24bae701487245900cd
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-01-22 15:11:01 +0100

    skapa student-card

--- student-card/student.js
+++ student-card/student.js
@@ -27,7 +27,22 @@ $(() => {
         this.social =social;
     }
 
-    
+    let zlatan = new Student(
+        "1",
+        "Zlatan Ibrahimovic",
+        "41",
+        "Milano, Italy",
+        "Profesional football player",
+        "Swedish, english, italian",
+        "Hi! I’m a 41-year-old retired professional football player on a mission to turn my new passion into a career far from the football pitch. Currently studying my second year at Changemaker Educations to become a web developer and are now ready to take the next step and put my new skills to the test in the real world! I'm a very dedicated person and determined to reach every goal I set.",
+        "My dream is to secure my first job in the tech industry, where I can apply my expertise in HTML, CSS and JavaScript to create user-friendly, visually appealing websites.",
+        "HTML/CSS, JavaScript, WordPress",
+        "z.ibrahimovic@cmeducation.se",
+        "0731231212",
+        "facebook"
+    )
+
+
   
    
   });
