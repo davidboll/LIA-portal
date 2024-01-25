dbfeb2e3603e3ad398f5b99af2c8d26c27d0284a
shirin faile <shirin.faile@hotmail.com>
2024-01-25 12:31:35 +0100

    added banner to index.html and made it look good on desktop/media-query

--- scripts/student-slider.js
+++ scripts/student-slider.js
@@ -1,28 +1,33 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+$(() => {
+  let slideIndex = 1;
+  showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-  
+
+  function showSlides(n) {
+    let slides = $(".mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    
+    slides.hide();
+    slides.eq(slideIndex - 1).show();
+  }
+
+  $(".prev").on("click", () => {
+    plusSlides(-1);
+  });
+
+  $(".next").on("click", () => {
+    plusSlides(1);
+  });
+});
+
+
+
   
