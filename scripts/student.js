814981c52fdfb5c7e61fc1c00bd23b90bccd7c13
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-02-01 13:50:33 +0100

    cleaned up the code a bit

--- scripts/student.js
+++ scripts/student.js
@@ -31,68 +31,76 @@ $(() => {
         this.image = image;
     }
 
-    Student.prototype.createStudentContent = function () {
+    const createStudentContent = student => {
+        $('.sidebar').empty();
+        $('.bio, .goals, .skills').empty();
         $('.bio').append(`
-        <p>${this.bio}</p>
+        <h5>Bio</h5>
+        <p>${student.bio}</p>
         `);
 
         $('.goals').append(`
-        <p>${this.goals}</p>`);
+        <h5>Goals</h5>
+        <p>${student.goals}</p>`);
 
         $('.skills').append(`
+        <h5>Skills</h5>
         <p>Languages we have studied so far:</p>
         <ul>
-            <li>${this.skillOne}</li>
-            <li>${this.skillTwo}</li>
-            <li>${this.skillThree}</li>
+            <li>${student.skillOne}</li>
+            <li>${student.skillTwo}</li>
+            <li>${student.skillThree}</li>
         <ul> 
         `);
 
         $('.sidebar').append(`
         <div class="img-wrapper">
-            <img class="img" src="${this.image}" alt="avatar">
+            <img class="img" src="${student.image}" alt="avatar">
         </div>
         <div class="info-content">
-            <h4>${this.name}</h4>
-            <p><b>Age:</b> ${this.age}</p>
-            <p><b>From:</b> ${this.location}</p>
-            <p><b>Education:</b> ${this.education}</p>
-            <p><b>Language:</b> ${this.languages}</p>
+            <h4>${student.name}</h4>
+            <p><b>Age:</b> ${student.age}</p>
+            <p><b>From:</b> ${student.location}</p>
+            <p><b>Education:</b> ${student.education}</p>
+            <p><b>Language:</b> ${student.languages}</p>
             <h4>Contact Me</h4>
-            <p>${this.phone}</p>
-            <p>${this.email}</p>
+            <p>${student.phone}</p>
+            <p>${student.email}</p>
         </div>
         `);
     }
-
-    $('.student-ava').on("click", (e) => {
-        // Check if the popup is not visible before showing it
-        if (!$('.student-popup').hasClass('activePopup')) {
-            showPopup();
-        }
-        e.stopPropagation(); // Prevent the click event from propagating to document
-    });
-
-    // Bind a click event to the entire document
-    $(document).on('click', (e) => {
-        // Check if the clicked element is not part of the active popup
-        if (!$(e.target).closest('.activePopup').length) {
-            hidePopup();
-        }
-    });
-
-    function showPopup() {
-        $(".student-popup").show().addClass('activePopup'); // Add a class to identify the active popup
-        $('.student-ava').hide();
+ 
+    function showPopup(student) {
+      // e.stopPropagation(); // Prevent the click event from propagating to document
+        $(".student-popup-overlay").show().addClass('activePopup').css("display","flex"); // Add a class to identify the active popup
+        createStudentContent(student);
     }
+
+    $(".student-popup-overlay").on("click", hidePopup)
 
     function hidePopup() {
-        $(".student-popup").hide().removeClass('activePopup'); // Remove the class on hide
-        $('.student-ava').show();
+        $(".student-popup-overlay").hide().removeClass('activePopup'); // Remove the class on hide
     }
 
-   /*  let student1 = new Student(
-        "1",
+    const studentArray = [
+    new Student(
+        1,
+        "Anna Stewart",
+        "41",
+        "Milano, Italy",
+        "Frontend Developer",
+        "Swedish, english, italian",
+        "Hi! I'm a 41-year-old retired professional football player on a mission to turn my new passion into a career far from the football pitch. Currently studying my second year at Changemaker Educations to become a web developer and are now ready to take the next step and put my new skills to the test in the real world! I'm a very dedicated person and determined to reach every goal I set.",
+        "My dream is to secure my first job in the tech industry, where I can apply my expertise in HTML, CSS and JavaScript to create user-friendly, visually appealing websites.",
+        "HTML/CSS",
+        "Javascript",
+        "React",
+        "z.ibrahimovic@cmeducation.se",
+        "0731231212",
+        "../images/Student-slider/student1.png"
+    ),
+     new Student(
+        2,
         "John Doe",
         "23",
         "New York, USA",
@@ -105,11 +113,10 @@ $(() => {
         "Node.js",
         "john.doe@example.com",
         "555-1234",
-        "./images/student1.png"
-    );
-    
-    let student2 = new Student(
-        "2",
+        "../images/Student-slider/student2.png"
+    ),
+    new Student(
+        3,
         "Alice Johnson",
         "22",
         "London, UK",
@@ -122,12 +129,11 @@ $(() => {
         "Sketch",
         "alice.johnson@example.com",
         "555-5678",
-        "./images/student2.png"
-    );
-    
-    let student3 = new Student(
-        "3",
-        "Bob Smith",
+        "../images/Student-slider/student3.png"
+    ),
+    new Student(
+        4,
+        "Ahmed Hassan",
         "25",
         "San Francisco, USA",
         "Data Scientist",
@@ -139,11 +145,10 @@ $(() => {
         "SQL",
         "bob.smith@example.com",
         "555-9876",
-        "./images/student3.png"
-    );
-    
-    let student4 = new Student(
-        "4",
+        "../images/Student-slider/student4.png"
+    ),
+    new Student(
+        5,
         "Eva Martinez",
         "24",
         "Barcelona, Spain",
@@ -156,12 +161,11 @@ $(() => {
         "React",
         "eva.martinez@example.com",
         "555-4321",
-        "./images/student4.png"
-    );
-    
-    let student5 = new Student(
-        "5",
-        "Michael Chen",
+        "../images/Student-slider/student5.png"
+    ),
+    new Student(
+        6,
+        "Mia Johnson",
         "22",
         "Beijing, China",
         "AI Researcher",
@@ -173,12 +177,11 @@ $(() => {
         "PyTorch",
         "michael.chen@example.com",
         "555-8765",
-        "./images/student5.png"
-    );
-    
-    let student6 = new Student(
-        "6",
-        "Sophia Kim",
+        "../images/Student-slider/student6.png"
+    ),
+    new Student(
+        7,
+        "Bob Smith",
         "23",
         "Seoul, South Korea",
         "Cybersecurity Analyst",
@@ -190,12 +193,11 @@ $(() => {
         "Incident Response",
         "sophia.kim@example.com",
         "555-2345",
-        "./images/student6.png"
-    );
-    
-    let student7 = new Student(
-        "7",
-        "Ahmed Hassan",
+        "../images/Student-slider/student7.png"
+    ),
+    new Student(
+        8,
+        "Sophia Kim",
         "26",
         "Cairo, Egypt",
         "Mobile App Developer",
@@ -207,12 +209,11 @@ $(() => {
         "Swift",
         "ahmed.hassan@example.com",
         "555-6789",
-        "./images/student7.png"
-    );
-    
-    let student8 = new Student(
-        "8",
-        "Mia Johnson",
+        "../images/Student-slider/student8.png"
+    ),
+    new Student(
+        9,
+        "Michael Chen",
         "21",
         "Toronto, Canada",
         "Game Developer",
@@ -224,11 +225,10 @@ $(() => {
         "3D Modeling",
         "mia.johnson@example.com",
         "555-5432",
-        "./images/student8.png"
-    );
-    
-    let student9 = new Student(
-        "9",
+        "../images/Student-slider/student9.png"
+    ),
+    new Student(
+        10,
         "Luca Rossi",
         "24",
         "Rome, Italy",
@@ -241,11 +241,10 @@ $(() => {
         "Hyperledger Fabric",
         "luca.rossi@example.com",
         "555-7654",
-        "./images/student9.png"
-    );
-    
-    let student10 = new Student(
-        "10",
+        "../images/Student-slider/student10.png"
+    ),
+    new Student(
+        11,
         "Yuki Tanaka",
         "22",
         "Tokyo, Japan",
@@ -258,11 +257,10 @@ $(() => {
         "Community Engagement",
         "yuki.tanaka@example.com",
         "555-8765",
-        "./images/student10.png"
-    );
-    
-    let student11 = new Student(
-        "11",
+        "../images/Student-slider/student11.png"
+    ),
+    new Student(
+        12,
         "Emma Anderson",
         "25",
         "Stockholm, Sweden",
@@ -275,29 +273,19 @@ $(() => {
         "Node.js",
         "emma.anderson@example.com",
         "555-9876",
-        "./images/student11.png"
-    ); */
+        "../images/Student-slider/student12.png"
+    ), 
+    ];
 
-    let zlatan = new Student(
-        "12",
-        "Zlatan Ibrahimovic",
-        "41",
-        "Milano, Italy",
-        "Frontend Developer",
-        "Swedish, english, italian",
-        "Hi! I'm a 41-year-old retired professional football player on a mission to turn my new passion into a career far from the football pitch. Currently studying my second year at Changemaker Educations to become a web developer and are now ready to take the next step and put my new skills to the test in the real world! I'm a very dedicated person and determined to reach every goal I set.",
-        "My dream is to secure my first job in the tech industry, where I can apply my expertise in HTML, CSS and JavaScript to create user-friendly, visually appealing websites.",
-        "HTML/CSS",
-        "Javascript",
-        "React",
-        "z.ibrahimovic@cmeducation.se",
-        "0731231212",
-        "./images/student.png"
-    ) 
+    studentArray.forEach(student => {
+        let studentElement = `
+            <div class="student-ava" >
+            <img src=${student.image} alt="student">
+            <h4>${student.name}</h4>
+            </div>
+        `
+        $('.student-wrapper').append($(studentElement).on("click", () => showPopup(student)));
+    }) 
     
 
-    let studentArray = [zlatan];
-
-    studentArray.forEach((student) => student.createStudentContent());
-   
   });
